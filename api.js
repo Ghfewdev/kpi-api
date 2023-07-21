c2faea9db49ef87ee3aeea15c59899ba2f68bba3
Ghfewdev <few_za75@hotmail.com>
2023-07-21 16:21:39 +0700

    api

---
+++ api.js
@@ -0,0 +1,250 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json();
+const mysql = require('mysql2')
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'test1'
+})
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken')
+const secect = 'abcdefg'
+
+app.use(cors());
+
+// users
+app.post('/register', jsonParser, (req, res, next) => {
+    bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+        var Isql = "INSERT INTO users (us_email, us_password, us_name, us_agency) VALUES (?, ?, ?, ?)"
+        var IV = [req.body.email, hash, req.body.name, req.body.agency]
+        conn.execute(Isql, IV, (err, results, fields) => {
+            if (err) {
+                res.json({ status: 'error', massage: err })
+                return
+            } else
+                res.json({ status: 'ok' })
+
+        })
+
+    });
+
+})
+
+app.post('/login', jsonParser, (req, res, next) => {
+    var qsql = "SELECT * FROM users WHERE us_name = ?"
+    var qy = req.body.name
+    conn.execute(qsql, [qy], (err, users, fields) => {
+        if (err) { res.json({ status: 'error', massage: err }); return }
+        if (users.length === 0) { res.json({ status: 'error', massage: 'no user not found' }); return }
+        bcrypt.compare(req.body.password, users[0].us_password, (err, islogin) => {
+            if (islogin) {
+                var token = jwt.sign({ name: users[0].us_name }, secect, { expiresIn: '1h' })+ "$" + users[0].us_level;
+                res.json({ status: 'ok', massage: 'login success', token, name: users[0].us_name, id: users[0].us_id, agency: users[0].us_agency });
+            } else {
+                res.json({ status: 'erorr', massage: 'login failed' })
+            }
+        })
+    })
+})
+
+app.post('/authen', jsonParser, (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        const decoded = jwt.verify(token, secect);
+        res.json({ status: 'ok', name: decoded.name, decoded });
+    } catch (err) {
+        res.json({ status: 'error', massage: err.message })
+    }
+
+});
+
+app.get('/users', (req, res) => {
+    conn.query("SELECT * FROM users ", (err, results, fields) => {
+        res.send(results)
+    })
+});
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id
+    conn.query("SELECT * FROM users WHERE us_id = ?", [id], (err, results, fields) => {
+        res.send(results)
+    })
+});
+
+// form
+app.post('/form/add', jsonParser, (req, res, next) => {
+    var Isql = "INSERT INTO form (fm_id, fm_name, fm_solve,fm_method, fm_define, fm_paras, fm_numpara) VALUES (?, ?, ?, ?, ?, ?, ?)"
+    var IV = [req.body.id, req.body.name, req.body.solve, req.body.method, req.body.def, req.body.paras, req.body.numpara]
+    conn.execute(Isql, IV, (err, results, fields) => {
+        if (err) {
+            res.json({ status: 'error', massage: err })
+            return
+        } else
+            res.json({ status: 'ok' })
+
+    })
+
+})
+
+app.get("/form", jsonParser, (req, res, next) => {
+    conn.query("SELECT * FROM form", (err, form, fields) => {
+        res.send(form);
+    })
+})
+
+app.get("/form/:id", jsonParser, (req, res, next) => {
+    const id = req.params.id
+    conn.query("SELECT * FROM form WHERE fm_id = ?", [id], (err, form, fields) => {
+        res.send(form);
+    })
+})
+
+//detail
+app.post('/form/fill', jsonParser, (req, res, next) => {
+    var Isql = "INSERT INTO detail (fm_id, de_qur, de_paras, de_ans, de_result) VALUES (?, ?, ?, ?, ?)"
+    var IV = [req.body.formid, req.body.qur, req.body.paras, req.body.ans, req.body.result]
+    conn.execute(Isql, IV, (err, results, fields) => {
+        if (err) {
+            res.json({ status: 'error', massage: err })
+            return
+        } else
+            res.json({ status: 'ok' })
+
+    })
+
+})
+
+app.get("/detail", jsonParser, (req, res, next) => {
+    conn.query("SELECT * FROM detail", (err, detail, fields) => {
+        res.send(detail);
+    })
+})
+
+
+app.get("/detail/:id", jsonParser, (req, res, next) => {
+    const id = req.params.id;
+    conn.query("SELECT * FROM detail WHERE de_id = ?", [id], (err, detail, fields) => {
+        res.send(detail);
+    })
+})
+
+
+//formed
+app.post('/formed/fill', jsonParser, (req, res, next) => {
+    const date = `${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate()}`
+    const time = `${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`
+    var Isql = "INSERT INTO formed (us_id, de_id, fd_date, fd_time) VALUES (?, ?, ?, ?)"
+    var IV = [req.body.user, req.body.detail, date, time]
+    conn.execute(Isql, IV, (err, results, fields) => {
+        if (err) {
+            res.json({ status: 'error', massage: err })
+            return
+        } else
+            res.json({ status: 'ok' })
+    })
+})
+
+app.get("/formed", jsonParser, (req, res, next) => {
+    conn.query("SELECT * FROM formed", (err, formed, fields) => {
+        formed = formed.map(d => {
+            d.fd_date = d.fd_date.toISOString().split('T')[0];
+            return d;
+        })
+        res.send(formed)
+    })
+})
+
+app.get("/formed/:id", jsonParser, (req, res, next) => {
+    const id = req.params.id
+    conn.query("SELECT * FROM formed WHERE fd_id = ?", [id], (err, formed, fields) => {
+        res.send(formed)
+    })
+})
+
+//all
+app.get("/all", jsonParser, (req, res, next) => {
+    conn.query("SELECT * FROM detail RIGHT JOIN formed ON detail.de_id = formed.de_id RIGHT JOIN users ON formed.us_id = users.us_id RIGHT JOIN form on form.fm_id = detail.fm_id;",
+        (err, all, fields) => {
+            all = all.map(d => {
+                if (d.fd_date != null)
+                    d.fd_date = d.fd_date.toISOString().split('T')[0];
+                return d;
+            })
+            res.send(all);
+        })
+})
+
+app.get("/all/:id", jsonParser, (req, res, next) => {
+    const id = req.params.id
+    conn.query("SELECT * FROM detail RIGHT JOIN formed ON detail.de_id = formed.de_id RIGHT JOIN users ON formed.us_id = users.us_id RIGHT JOIN form on form.fm_id = detail.fm_id WHERE detail.fm_id = ?",
+        [id], (err, all, fields) => {
+            all = all.map(d => {
+                if (d.fd_date != null)
+                    d.fd_date = d.fd_date.toISOString().split('T')[0];
+                return d;
+            })
+            res.send(all);
+        })
+})
+
+app.get("/all/user/:id", jsonParser, (req, res, next) => {
+    const id = req.params.id
+    conn.query("SELECT * FROM detail RIGHT JOIN formed ON detail.de_id = formed.de_id RIGHT JOIN users ON formed.us_id = users.us_id RIGHT JOIN form on form.fm_id = detail.fm_id WHERE users.us_id = ?",
+        [id], (err, all, fields) => {
+            all = all.map(d => {
+                if (d.fd_date != null)
+                    d.fd_date = d.fd_date.toISOString().split('T')[0];
+                return d;
+            })
+            res.send(all);
+        })
+})
+
+//report
+app.get('/report/api', (req, res) => {
+
+    const page = parseInt(req.query.page);
+    const per_page = parseInt(req.query.per_page);
+    const sort_collumn = req.query.sort_collumn;
+    const sort_direction = req.query.sort_direction;
+    const quarter = req.query.quarter;
+    const search = req.query.search;
+
+    const start_idx = (page - 1) * per_page;
+    var params = [];
+    var sql = "SELECT * FROM detail RIGHT JOIN formed ON detail.de_id = formed.de_id RIGHT JOIN users ON formed.us_id = users.us_id RIGHT JOIN form on form.fm_id = detail.fm_id";
+    if (quarter) {
+        sql += " WHERE de_qur = ?"
+        params.push(quarter);
+    }
+    if (search) {
+        sql += " WHERE us_agency LIKE ? "
+        params.push("%" + search + "%");
+    }
+    if (sort_collumn) {
+        sql += " ORDER BY " + sort_collumn + " " + sort_direction;
+    }
+    sql += " LIMIT ?, ? ";
+    params.push(start_idx);
+    params.push(per_page);
+    
+    console.log(sql, params);
+    conn.execute(sql, params, (err, tables, fields) => {
+        tables = tables.map(d => {
+            if (d.fd_date != null)
+                d.fd_date = d.fd_date.toISOString().split('T')[0];
+            return d;
+        })
+        console.log(tables)
+        res.json(tables)
+    })
+});
+
+const Port = process.env.Port || 3000
+app.listen(Port, jsonParser, () => {
+    console.log(`start server on Port ${Port}`)
+})

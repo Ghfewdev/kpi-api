b67c08107fdb15bf4105c04b66aec8c1c2dd55e6
Ghfewdev <few_za75@hotmail.com>
2024-11-29 11:07:29 +0700

    pool

--- api.js
+++ api.js
@@ -5,7 +5,7 @@ const app = express();
 const bodyParser = require('body-parser')
 const jsonParser = bodyParser.json();
 const mysql = require('mysql2')
-const conn = mysql.createConnection({
+const conn = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
